36333e1823f31d6925bcf64e3b00ed011711a443
Julia Karolina <juliakarolina24@gmail.com>
2022-08-10 21:41:54 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: const Text("I'm Rich"),
-        backgroundColor: Colors.blueGrey[900],
-      ),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text("I'm Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
         body: const Center(
           child: Image(
-            image: AssetImage('images/dimond.png')
+              image: AssetImage('images/dimond.png')
           ),
         )
     ),
